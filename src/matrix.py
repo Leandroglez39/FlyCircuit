66191a105637fab8d0de2b42d8fcb9db5abbf20a
Leo <leandroglez39@gmail.com>
2023-01-26 10:22:12 +0100

    update

--- src/matrix.py
+++ src/matrix.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 from dataclasses import dataclass
 import networkx as nx
-
+import os
 
 @dataclass
 class Matrix:
@@ -11,13 +11,14 @@ class Matrix:
     pos_code_nodes: dict
     G = nx.DiGraph()
     ady_list : list 
+    G_small = nx.DiGraph()
     
     def __post_init__(self):
         
         self.list_nodes = pickle.load(open('./data/nodes.pkl', 'rb'))
         self.pos_code_nodes = pickle.load(open('./data/pos_code_nodes.pkl', 'rb'))
         self.ady_list = [[] for _ in range(len(self.list_nodes))]
-        self.read_adym
+        #self.read_adym
 
     def insert_nodes(self):
         self.G.add_nodes_from(self.list_nodes)
@@ -27,16 +28,103 @@ class Matrix:
             for j in range(len(self.ady_list[i])):
                 y = self.ady_list[i][j][0]
                 weight = self.ady_list[i][j][1]
-                self.G.add_edge(self.list_nodes[i], self.list_nodes[y], weight=weight)
+                self.G.add_edge(self.list_nodes[y], self.list_nodes[i], weight=weight)
+    
+    def export_graph_to_csv(self):
+        nx.write_edgelist(self.G, "30set.csv", delimiter=",", data=['weight'])
+
+    def export_graph_to_csv_size(self, size):
+
+        G = nx.DiGraph()
+        G.add_nodes_from(self.list_nodes[:size])
+
+        df = pd.read_csv('./data/matrix/0 file.csv')
+        
+
+        ady_list = [[] for _ in range(size)]
+
+        for i in range(len(self.list_nodes[:size])):
+            row = df.loc[i].to_list()
+            for j in range(size):
+                if row[j] != 0:
+                    ady_list[i].append((j, row[j]))
+
+        for i in range(len(ady_list)):
+            for j in range(len(ady_list[i])):
+                y = ady_list[i][j][0]
+                weight = ady_list[i][j][1]
+                G.add_edge(self.list_nodes[y], self.list_nodes[i], weight=weight)
+
+        self.G_small = G
+
+        nx.write_edgelist(G, f"0set_size{str(size)}.csv", delimiter=",", data=['weight'])
+        nx.write_gexf(G, f"0set_size{str(size)}.gexf")
+    
+    def export_graph_to_graphml(self, path = "graph_19k_3.5m.gml"):
+        nx.write_graphml(self.G, path)
+
+    def export_graph_to_adjlist(self, path = "graph_19k_3.5m.adyl"):
+        nx.write_adjlist(self.G, path)
+
+    def load_ady_matrix(self, count = 0):
+
+        for x in range(count+1):
+            print(f'Loading: {x} file.csv')
+            df = pd.read_csv(f'./data/matrix/{x} file.csv')            
+            for i in range(df.shape[0]):
+                row = df.loc[i].to_list()
+                for j in range(len(row)):
+                    if row[j] != 0:
+                        self.ady_list[i + x * 642].append((j, row[j]))
+            print(f'Finished: {x} file.csv')
+
+
+        with open(f'./data/adym_{count}.pkl', 'wb') as f:
+            pickle.dump(self.ady_list, f)
+   
+    def sava_matrix_obj(self, path = './data/graph_19k_3.5m.pkl'):
+        
+        with open(path, 'wb') as f:
+            pickle.dump(self.G, f)
+
+    def load_matrix_obj(self, path = './data/graph_19k_3.5m.pkl'):
+        self.G = pickle.load(open(path, 'rb'))
 
     @property
     def read_adym(self):
-        self.ady_list = pickle.load(open('./data/adym_0.pkl', 'rb'))
+        self.ady_list = pickle.load(open('./data/adym_30.pkl', 'rb'))
         
 
 if __name__ == '__main__':
+    
     m = Matrix([], {},[])
-    m.insert_nodes()
-    m.insert_weighted_edges()
-    for node in m.list_nodes:
-        print(f'{node} : {m.G.degree[node]}')
+    m.load_matrix_obj()
+    m.export_graph_to_adjlist()
+    # m.insert_nodes()
+    # m.read_adym
+    #m.load_ady_matrix(30)    
+    # m.insert_weighted_edges()
+    # m.sava_matrix_obj()
+    print(m.G.number_of_edges())
+    #m.export_graph_to_csv()
+    # m.load_ady_matrix(30)
+    # for node in m.list_nodes:
+    #     count += 1
+    #     if count < 10:
+    #         print(f'{node} : {m.G.degree[node]}')
+    #         print(f'{node} : {m.G.adj[node]}')
+    #     else:
+    #         break
+    #nx.write_gexf(m.G, "0set.gexf")
+    
+    #m.export_graph_to_csv_size(500)
+    #print(m.G.number_of_edges())
+    # print(len(list(m.G.neighbors('104198-F-000000'))))
+
+    # print(len(list(nx.strongly_connected_components(m.G))))
+
+    # from cdlib import algorithms
+
+    # coms = algorithms.infomap(m.G_small)
+
+    # print(coms.communities)
