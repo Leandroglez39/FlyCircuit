54cd9868c3743dc9df9b6c584c4c6124d8e1fd84
horacio <dhgrass@unav.es>
2023-01-30 16:33:47 +0100

    update bash to install env

--- src/install.bash
+++ src/install.bash
@@ -1,9 +1,9 @@
 python3 -m pip install virtualenv
 virtualenv envCDLib
 virtualenv envNX
-source envCDLib/bin/acivate
+source envCDLib/bin/activate
 pip install -r cdlib_requirements.txt
 dactivate
-source envCDLib/bin/acivate
+source envCDLib/bin/activate
 pip install -r nx_requirements.txt
 dactivate
